a84a9dfb8d330f382a84b4acf1fc865e94ef67c1
hamza1886 <hamza1886@gmail.com>
2021-03-04 13:23:37 +0500

    Refactored UserController to AuthController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('register');
+    }
+
+    /**
+     * Register a new user
+     *
+     * @param RegisterUserRequest $request
+     * @return JsonResponse
+     */
+    public function register(RegisterUserRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->date_of_birth = $request->date_of_birth;
+        $user->save();
+
+        return response()->json([
+            'data' => [],
+            'message' => 'Success registering user',
+            'code' => 201,
+        ], 201);
+    }
+
+    /**
+     * Login an existing user
+     *
+     * @param LoginUserRequest $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function login(LoginUserRequest $request)
+    {
+        $user = (new User)->where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'data' => [
+                'token' => $user->createToken($request->device_name)->plainTextToken,
+            ],
+            'message' => 'Success logging-in user',
+            'code' => 200,
+        ]);
+    }
+
+    /**
+     * Logout user from current device
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $tokenId = explode('|', $request->bearerToken());
+        $request->user('auth:santum')->tokens()->where('id', $tokenId)->delete();
+
+        return response()->json([
+            'data' => [],
+            'message' => 'Success logout user from current device',
+            'code' => 200,
+        ]);
+    }
+
+    /**
+     * Logout user from all devices
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout_all(Request $request)
+    {
+        $request->user('auth:santum')->tokens()->delete();
+
+        return response()->json([
+            'data' => [],
+            'message' => 'Success logout user from all device',
+            'code' => 200,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BookController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [UserController::class, 'register'])->name('users.register');
-Route::post('/login', [UserController::class, 'login'])->name('users.login');
-Route::post('/logout', [UserController::class, 'logout'])->name('users.logout');
-Route::post('/logout_all', [UserController::class, 'logout_all'])->name('users.logout_all');
+Route::post('/register', [AuthController::class, 'register'])->name('users.register');
+Route::post('/login', [AuthController::class, 'login'])->name('users.login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('users.logout');
+Route::post('/logout_all', [AuthController::class, 'logout_all'])->name('users.logout_all');
 
 Route::get('/user', [UserController::class, 'show'])->name('users.show');
 
