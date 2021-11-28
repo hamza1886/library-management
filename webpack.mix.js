6d3adf14b0057803f3f97af49cb2ef09cb08e523
Hamza Rashid <hamza1886@gmail.com>
2021-11-28 14:57:33 +0500

    Updated laravel-mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .sourceMaps(false)
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .when(mix.inProduction(), function (mix) {
+        mix.version()
+            .minify('public/css/app.css', 'public/css/app.min.css')
+            .minify('public/js/app.js', 'public/js/app.min.js');
+    })
+    .disableNotifications();
